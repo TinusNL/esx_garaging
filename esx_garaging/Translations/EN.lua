f737419af52cf9e13d8a78deba6893f3772d0c03
Tinus_NL <66055492+TinusNL@users.noreply.github.com>
2021-03-08 20:06:27 +0100

    Update EN.lua

--- esx_garaging/Translations/EN.lua
+++ esx_garaging/Translations/EN.lua
@@ -9,18 +9,20 @@
 
 Translations['EN'] = {
     ["BUY_GARAGE"] = "Press ~INPUT_CONTEXT~ to buy the garage!",
-	["MENU_BUY"] = "Buying Garage",
-	["YES_BUY"] = "Yes",
-	["NO_BUY"] = "No",
-	["MONEY_BUY"] = "You ~r~don't~w~ have enough money! You need: ~r~€",
-	["SUCCES_BUY"] = "The garage has ~g~succesfully~w~ been bought!",
+    ["MENU_BUY"] = "Buying Garage",
+    ["MONEY_BUY"] = "You ~r~don't~w~ have enough money! You need: ~r~€",
+    ["SUCCES_BUY"] = "The garage has ~g~succesfully~w~ been bought!",
 
     ["ENTER_GARAGE"] = "Press ~INPUT_CONTEXT~ to enter the garage!",
     ["LOADING_ENTER_GARAGE"] = "Entering garage...",
     ["LEAVE_GARAGE"] = "Press ~INPUT_CONTEXT~ to leave the garage!",
     ["LOADING_LEAVE_GARAGE"] = "Leaving garage...",
 
-    ["SPAWN_ERROR"] = "~r~Something went wrong while spawning the vehicle!",
+    ["YES"] = "Yes",
+    ["NO"] = "No",
+
+    ["ERROR_SPAWN"] = "~r~Something went wrong while spawning the vehicle!",
+    ["BLOCK_SPAWN"] = "~r~The garage door is blocked! Make sure there is enough space to spawn your vehicle.",
 
     ["NO_SPACE"] = "There is ~r~no~w~ space in this garage!",
 
@@ -28,7 +30,11 @@ Translations['EN'] = {
 
     ["OPEN_LAPTOP"] = "Press ~INPUT_CONTEXT~ to open the laptop!",
     ["NAME_LAPTOP"] = 'NOT FOUND',
-    ["MENU_LAPTOP"] = "Vehicle Laptop",
+    ["MENU_LAPTOP"] = "Garage Laptop",
+    ["IMPOUND_LAPTOP"] = "Impound",
+    ["SELL_LAPTOP"] = "Sell Garage",
+    ["SUCCES_LAPTOP"] = "The garage has ~g~succesfully~w~ been sold!",
+    ["EMPTY_LAPTOP"] = "You first ~r~need~w~ to empty the garage!",
     ["MONEY_LAPTOP"] = "You ~r~don't~w~ have enough money! You need: ~r~€",
     ["SUCCES_LAPTOP"] = "The vehicle has ~g~succesfully~w~ been returned to your garage!",
 }
